74ee6f455ddda80fd3067f7aa1fd68d3a1c150f6
Vipinbrd <vipinbarode7@gmail.com>
2024-05-05 10:10:31 +0530

    persistence.xml added

---
+++ Hibernate practice project/hibernat01/src/main/java/hibernat01/utility/ConnectionProvider.java
@@ -0,0 +1,17 @@
+package hibernat01.utility;
+
+import java.sql.Connection;
+
+public class ConnectionProvider {
+	
+	
+	
+	public static Connection getConnection() {
+		
+		
+		return null;
+		
+		
+	}
+
+}
